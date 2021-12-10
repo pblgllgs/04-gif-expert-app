5d96277cf31fb7ac191e299ce4e9f781ec719f2d
pbl.gllgs <pbl.gllgs@gmail.com>
2021-12-10 18:01:33 -0300

    last updates

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 export const getGifs = async (category) => {
     const apiKey = "Ftmx0ADU6kkfDNb7tRGbwbieRvlpdZc0";
     const resp = await fetch(
-        `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURI(category)}&limit=10`,
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURI(category)}&limit=10`,
         {'mode': 'no-cors'}
         );
     const { data } = await resp.json();
